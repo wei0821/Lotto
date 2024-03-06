a04b5781551500dc0524ce2f0217cbfcf8aee8eb
wei0821 <harrylin149@gmail.com>
2024-03-06 17:55:24 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/s1114643/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114643/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.s1114643.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,11 @@ class MainActivity : AppCompatActivity() {
 
         var t: TextView = findViewById(R.id.txv)
         t.text = "0"
+
+        fun happy(v: View){
+            var txv: TextView = findViewById(R.id.txv)
+            txv.text = (1..100).random().toString()
+        }
 
     }
 }
